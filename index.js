1a83e4d754052db1c68c58c84ecf0eb26add4969
Agnessa Tund <agnessatnd@users.noreply.github.com>
2024-10-03 15:31:53 +0300

    Faili salvestamine lisatud

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const dtEt = require("./dateTime.js");
+const fs = require("fs");
+//paringu lahti harutamiseks POST meetodil
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+//paringu URL-i parsimine, falsee kui ainult tekst, true kui muud ka
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", (req, res)=>{
     //res.send("Express läks täiesti käima!");'
-    res.render("index.ejs");
+    res.render("index");
 });
 
 app.get("/timenow", (req, res)=>{
@@ -15,6 +20,45 @@ app.get("/timenow", (req, res)=>{
     const dateNow = dtEt.dateEt();
     const timeNow = dtEt.timeEt();
     res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow});
+});
+
+app.get("/justlist", (req, res)=>{
+    let folkWisdom = [];
+    fs.readFile("public/text_files/vanasonad.txt", "utf-8", (err, data)=>{
+        if(err){
+            //throw err;
+            res.render("justlist", {h2: "Vanasonad", listData: ["Ei leidnud uhtegi vanasona"]});
+        }
+        else {
+            folkWisdom = data.split(";");
+            res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom});
+        }
+    });
+});
+
+app.get("/regvisit", (req, res)=>{
+    res.render("regvisit");
+});
+
+app.post("/regvisit", (req, res)=>{
+    console.log(req.body);
+    fs.open("public/text_files/visitorlog.txt", "a", (err, file)=>{
+        if(err){
+            throw err;
+        }
+        else {
+            fs.appendFile("public/text_files/visitorlog.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ":", (err)=>{
+                if(err){
+                    throw err;
+                }
+                else {
+                    console.log("Faili kirjutati!");
+                    res.render("regvisit");
+                }
+            });
+
+        }
+    });
 });
 
 app.listen(5114);
